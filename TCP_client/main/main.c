e26fb6647a725a764b4e090069a66de88d2cc085
SeaHi-Mo <seahi@foxmail.com>
2023-10-11 20:48:38 +0800

    frist commit

---
+++ TCP_client/main/main.c
@@ -0,0 +1,31 @@
+/**
+ * @file main.c
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-08-22
+ *
+ * @copyright Copyright (c) 2023
+ *
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <FreeRTOS.h>
+#include <task.h>
+#include "board.h"
+#include "log.h"
+#include "wifi_event.h"
+
+#define DBG_TAG "MAIN"
+
+
+int main(void)
+{
+    board_init();
+    tcpip_init(NULL, NULL);
+    wifi_start_firmware_task(); 
+    
+    vTaskStartScheduler();
+}
